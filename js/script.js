b1a44869c7c1935efd59865e504460b195d7e949
Serhii Kondratenko <k.serjio@ukr.net>
2023-05-02 22:51:08 +0300

    +++

--- js/script.js
+++ js/script.js
@@ -946,3 +946,98 @@
 // console.log(includes(["apple", "plum", "pear", "orange"], "plum"))
 // console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"))
   
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   const keys = [];
+//   const values = [];
+//   // Change code below this line
+//   for (let key in apartment) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+
+//   console.log(keys);
+//   console.log(values);
+
+// const keys = [];
+// const values = [];
+
+// const advert = {
+//   service: "apt",
+// };
+
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//     if (apartment.hasOwnProperty(key)) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// };
+
+//   // Change code above this line
+// };
+//  console.log(keys);
+//  console.log(values);
+
+
+// function countProps(object) {
+//     let propCount = 0;
+//     // Change code below this line
+//     for (const key in object) {
+//         // console.log(object[key]);
+//         if (object.hasOwnProperty(key)) {
+//             propCount += 1;
+//         }
+//     };
+//     console.log(propCount);
+//     // Change code above this line
+//     return propCount;
+    
+//   }
+
+// countProps({})
+// countProps({ name: "Mango", age: 2 })
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })
+
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   const values = [];
+//   // Change code below this line
+
+//   const keys = Object.keys(apartment);
+
+//   for (const key of keys) {
+//     // console.log(key);
+//     // if (key.hasOwnProperty(apartment)) {
+//         values.push(apartment[key]);
+//     // }
+//   };
+//   console.log(keys);
+//   console.log(values);
+  
+function countProps(object) {
+    // Change code below this line
+    let propCount = 0;
+  
+    for (const key in object) {
+      if (object.hasOwnProperty(key)) {
+        propCount += 1;
+      }
+    }
+  
+    return propCount;
+    // Change code above this line
+  }
+  
