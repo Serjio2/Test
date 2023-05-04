3ef5d4f8fb64f6826b7e9e02f3cc9c6a655ab057
Serhii Kondratenko <k.serjio@ukr.net>
2023-05-05 01:13:41 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -1342,29 +1342,33 @@
 //   console.log(rgbColors);
 
 
-const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
   
-  function getProductPrice(productName) {
-    // Change code below this line
-    // productName = null;
-  for (const product of products) {
-    if (productName === product.name) {
-        return product.price;
-    } else if (productName === undefined ) {
-        return null;
-    } 
-  }
+//   function getProductPrice(productName) {
+//     // Change code below this line
+//     // productName = null;
+//   for (const product of products) {
+//     if (productName === product.name) {
+//         return product.price;
+//     }
+    
+//   }
+//   return null;
   
-    // Change code above this 
-  }
+//     // Change code above this 
+//   }
 
-console.log(getProductPrice("Radar"))
-console.log(getProductPrice("Grip"))
-console.log(getProductPrice("Scanner"))
-console.log(getProductPrice("Droid"))
-console.log(getProductPrice("Engine"))
+// console.log(getProductPrice("Radar"))
+// console.log(getProductPrice("Engine"))
+// console.log(getProductPrice("Grip"))
+// console.log(getProductPrice("Scanner"))
+// console.log(getProductPrice("Droid"))
+// console.log(getProductPrice("Engine"))
+
+
+
