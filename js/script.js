a240448d8a595466a87a121325a24dc5a1e9f7e2
Serhii Kondratenko <k.serjio@ukr.net>
2023-05-06 00:37:31 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -1415,32 +1415,170 @@
 
 
 
-const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
   
-  function calculateTotalPrice(productName) {
-    // Change code below this line
-    let totalPrice = 0;
+//   function calculateTotalPrice(productName) {
+//     // Change code below this line
+//     let totalPrice = 0;
 
-    for (const product of products) {
+//     for (const product of products) {
 
-        if (product.name === productName) {
-            totalPrice = product.price * product.quantity;
-        } 
-    }
+//         if (product.name === productName) {
+//             totalPrice = product.price * product.quantity;
+//         } 
+//     }
         
-    return totalPrice;
+//     return totalPrice;
   
-    // Change code above this line
-  }
+//     // Change code above this line
+//   }
 
-console.log(calculateTotalPrice("Blaster"))
-console.log(calculateTotalPrice("Radar"))
-console.log(calculateTotalPrice("Droid"))
-console.log(calculateTotalPrice("Grip"))
-console.log(calculateTotalPrice("Scanner"))
+// console.log(calculateTotalPrice("Blaster"))
+// console.log(calculateTotalPrice("Radar"))
+// console.log(calculateTotalPrice("Droid"))
+// console.log(calculateTotalPrice("Grip"))
+// console.log(calculateTotalPrice("Scanner"))
+
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const { yesterday, today, tomorrow } = highTemperatures;
+
+// // const yesterday = highTemperatures.yesterday;
+// // const today = highTemperatures.today;
+// // const tomorrow = highTemperatures.tomorrow;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(meanTemperature)
+
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const { yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+
+// // const yesterday = highTemperatures.yesterday;
+// // const today = highTemperatures.today;
+// // const tomorrow = highTemperatures.tomorrow;
+// // const icon = highTemperatures.icon;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(meanTemperature, icon);
+
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const { yesterday: highYesterday, today: highToday, tomorrow: highTomorrow, icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+
+// // const highYesterday = highTemperatures.yesterday;
+// // const highToday = highTemperatures.today;
+// // const highTomorrow = highTemperatures.tomorrow;
+// // const highIcon = highTemperatures.icon;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
   
+// console.log(meanTemperature, highIcon);
+
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   // const { hex, rgb } = colors;
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const { today: { high: highToday, low: lowToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg" }, tomorrow: { high: highTomorrow, low: lowTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} } = forecast;
+
+// // const highToday = forecast.today.high;
+// // const lowToday = forecast.today.low;
+// // const todayIcon = forecast.today.icon;
+
+// // const highTomorrow = forecast.tomorrow.high;
+// // const lowTomorrow = forecast.tomorrow.low;
+// // const tomorrowIcon = forecast.tomorrow.icon;
+
+// console.log(highToday, lowToday, todayIcon);
+// console.log(highTomorrow, lowTomorrow, tomorrowIcon);
+
+
+
+// Change code below this line
+function calculateMeanTemperature(forecast) {
+
+  // const forecast = {
+  //   today: {
+  //     low: 10,
+  //     high: 20
+  //   },
+  //   tomorrow: {
+  //     low: 20,
+  //     high: 30
+  //   },
+  // };
+
+  const { today: { high: todayHigh, low: todayLow }, tomorrow: { low: tomorrowLow, high: tomorrowHigh }, } = forecast;
+
+  // const todayLow = forecast.today.low;
+  // const todayHigh = forecast.today.high;
+  // const tomorrowLow = forecast.tomorrow.low;
+  // const tomorrowHigh = forecast.tomorrow.high;
+
+  // Change code above this line
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }))
+console.log(calculateMeanTemperature({ today: {low: 37, high: 40}, tomorrow: {low: 33, high: 38} }))
+
+
