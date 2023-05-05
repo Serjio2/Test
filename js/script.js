a43ee3f09c51aba6729b6e51c5fe84d46098eb2b
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-05-05 16:42:15 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -1371,4 +1371,76 @@
 // console.log(getProductPrice("Engine"))
 
 
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getAllPropValues(propName) {
+//     // Change code below this line
+//     const allPropertyValues = [];
 
+//     for (const product of products) {
+//         console.log(product[propName]);
+//             if (product[propName]) {
+//                 allPropertyValues.push(product[propName])
+//             }
+        
+//     }
+//     console.log(allPropertyValues);
+//     return allPropertyValues;
+//     // Change code above this line
+//   }
+
+// getAllPropValues("name")
+// getAllPropValues("quantity")
+// getAllPropValues("price")
+// getAllPropValues("category")
+
+// const a = {x:1, y:2};
+// const b = {x:0, z:3, name: "Mango"};
+
+// const c = {
+//     ...a,
+//     y:10,
+//     ...b,
+//     x:25,
+//     name: "Polli",
+
+// };
+// console.log(c);
+
+
+
+
+const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+  
+  function calculateTotalPrice(productName) {
+    // Change code below this line
+    let totalPrice = 0;
+
+    for (const product of products) {
+
+        if (product.name === productName) {
+            totalPrice = product.price * product.quantity;
+        } 
+    }
+        
+    return totalPrice;
+  
+    // Change code above this line
+  }
+
+console.log(calculateTotalPrice("Blaster"))
+console.log(calculateTotalPrice("Radar"))
+console.log(calculateTotalPrice("Droid"))
+console.log(calculateTotalPrice("Grip"))
+console.log(calculateTotalPrice("Scanner"))
+  
