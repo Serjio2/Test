af516c70431bf3cf5825dd53fd9660f1c59d1cf2
Serhii Kondratenko <k.serjio@ukr.net>
2023-05-10 23:44:53 +0300

    +++

--- js/script.js
+++ js/script.js
@@ -2049,4 +2049,119 @@
 // console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
 // console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
 // // console.log(onOrderError(5));
+
+
+// function calculateTotalPrice(orderedItems) {
+//     let totalPrice = 0;
+//     // Change code below this line
   
+//     // for (let i = 0; i < orderedItems.length; i += 1) {
+//     //   totalPrice += orderedItems[i];
+//     // }
+//     orderedItems.forEach((elem) => { 
+//         totalPrice += elem;
+//     });
+
+//     // Change code above this line
+//     return totalPrice;
+//   }
+  
+// console.log(calculateTotalPrice([12, 85, 37, 4]))
+// console.log(calculateTotalPrice([164, 48, 291]))
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
+
+
+// function filterArray(numbers, value) {
+//     const filteredNumbers = [];
+//     // Change code below this line
+  
+//     // for (let i = 0; i < numbers.length; i += 1) {
+//     //   if (numbers[i] > value) {
+//     //     filteredNumbers.push(numbers[i]);
+//     //   }
+//     // }
+
+//     numbers.forEach( (elem) => {
+//         if (elem > value) {
+//             filteredNumbers.push(elem);
+//         }
+//     });
+  
+//     // Change code above this line
+//     return filteredNumbers;
+//   }
+
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)) //повертає [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)) //повертає [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)) //повертає []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)) // повертає [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)) //повертає [24, 41, 76]
+
+
+// function getCommonElements(firstArray, secondArray) {
+//     const commonElements = [];
+//     // Change code below this line
+  
+//     // for (let i = 0; i < firstArray.length; i += 1) {
+//     //   if (secondArray.includes(firstArray[i])) {
+//     //     commonElements.push(firstArray[i]);
+//     //   }
+//     // }
+
+//     firstArray.forEach( (elem) => {
+//         if (secondArray.includes(elem)) {
+//             commonElements.push(elem);
+//         }
+//     });
+  
+//     return commonElements;
+//     // Change code above this line
+//   }
+  
+
+// console.log(getCommonElements([1, 2, 3], [2, 4])) //повертає [2]
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19])) //повертає [1, 2]
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])) //повертає [12, 27, 3]
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40])) //повертає [10, 30, 40]
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30])) //повертає []
+
+
+
+// Change code below this line
+
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//     // Change code above this line
+//     return quantity * pricePerItem;
+//   }
+
+
+// console.log(calculateTotalPrice(5, 100)) //повертає 500
+// console.log(calculateTotalPrice(8, 60)) //повертає 480
+// console.log(calculateTotalPrice(3, 400)) //повертає 1200
+
+
+// // Change code below this line
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+//   // Change code above this line
+
+
+// console.log(calculateTotalPrice(5, 100)) //повертає 500
+// console.log(calculateTotalPrice(8, 60)) //повертає 480
+// console.log(calculateTotalPrice(3, 400)) //повертає 1200
+
+
+// // Change code below this line
+// const calculateTotalPrice = (orderedItems) => {
+//     let totalPrice = 0;
+  
+//     orderedItems.forEach( (item) =>  totalPrice += item);
+
+//     return totalPrice;
+//   }
+//   // Change code above this line
+
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])) //повертає 138
+// console.log(calculateTotalPrice([164, 48, 291])) //повертає 503
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176])) //повертає 1116
