8ef346bb3a633fe2ad6f478a3470d091c63f5045
Serhii Kondratenko <k.serjio@ukr.net>
2023-05-16 00:07:43 +0300

    ++++

--- js/script.js
+++ js/script.js
@@ -3074,3 +3074,29 @@
 
 // console.table(getTotalBalanceByGender(users, "male"));
 // console.table(getTotalBalanceByGender(users, "female"));
+
+
+
+const pizzaPalace = {
+    pizzas: ["Supercheese", "Smoked", "Four meats"],
+    // Change code below this line
+    checkPizza(pizzaName) {
+      return this.pizzas.includes(pizzaName);
+    },
+    order(pizzaName) {
+      const isPizzaAvailable = this.checkPizza(pizzaName);
+  
+      if (!isPizzaAvailable) {
+        return `Sorry, there is no pizza named «${pizzaName}»`;
+      }
+  
+      return `Order accepted, preparing «${pizzaName}» pizza`;
+    },
+    // Change code above this line
+  };
+
+
+  console.log(pizzaPalace.order("Smoked"))
+  console.log(pizzaPalace.order("Four meats"))
+  console.log(pizzaPalace.order("Big Mike"))
+  console.log(pizzaPalace.order("Viennese"))
